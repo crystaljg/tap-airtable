19b03c4159d3fde2f6587da8adbdb2b60b41991c
Crystal Gong <cgong@actbluetech.com>
2021-09-17 10:22:44 -0400

    define discover to make catalog

--- tap_airtable/__init__.py
+++ tap_airtable/__init__.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import os
 import json
+import re
 import singer
 from singer import utils, metadata
 from singer.catalog import Catalog, CatalogEntry
 from singer.schema import Schema
 
+from pyairtable import Base
 
-REQUIRED_CONFIG_KEYS = ['api_token','base_id','tables']
+
+REQUIRED_CONFIG_KEYS = ['api_key', 'base_id', 'tables']
 LOGGER = singer.get_logger()
 
 
@@ -15,24 +18,96 @@ def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
 
-def load_schemas():
-    """ Load schemas from schemas folder """
-    schemas = {}
-    for filename in os.listdir(get_abs_path('schemas')):
-        path = get_abs_path('schemas') + '/' + filename
-        file_raw = filename.replace('.json', '')
-        with open(path) as file:
-            schemas[file_raw] = Schema.from_dict(json.load(file))
-    return schemas
+def normalize_name(field):
+    # lowercase, remove non-alpha chars, replace space with underscore
+    return re.sub("[^a-z ]+", "", field.lower()).replace(" ", "_")
 
 
-def discover():
-    raw_schemas = load_schemas()
+def get_schema(base, table):
+    """
+    creates a raw schema for the given table in the airtable base
+    """
+    schema = {
+        "type": ["null", "object"],
+        "additionalProperties": False,
+        "properties": {
+            "airtable_id": {"type": ["string"]},
+            "created_time": {
+                "anyOf": [
+                    {
+                        "type": ["null", "string"],
+                        "format": "date-time"
+                    },
+                    {
+                        "type": ["null", "string"]
+                    }
+                ]
+            }
+        }
+    }
+
+    table_data = base.all(table)
+
+    for row in table_data:
+        for field, value in row["fields"].items():
+
+            clean_field = normalize_name(field)
+
+            inferred_type = infer_type(value)
+            schema['properties'][clean_field] = {
+                "type": ["null", inferred_type]}
+
+    return Schema(schema)
+
+
+def infer_type(datum):
+    """
+    Returns the inferred data type
+    """
+    if datum is None or datum == '':
+        return None
+
+    try:
+        int(str(datum))
+        return 'integer'
+    except (ValueError, TypeError):
+        pass
+    try:
+        float(str(datum))
+        return 'number'
+    except (ValueError, TypeError):
+        pass
+
+    return 'string'
+
+
+def discover_schemas(config):
+    """
+    Creates a dict containing schema for each table given in the config
+    """
+    base = Base(config["api_key"], config["base_id"])
+    raw_schemas = {}
+
+    for table in config["tables"]:
+        stream = normalize_name(table)
+        schema = get_schema(base, table)
+
+        raw_schemas[stream] = schema
+
+    return raw_schemas
+
+
+def discover(config):
+    LOGGER.info("Starting discover")
+    raw_schemas = discover_schemas(config)
+
     streams = []
     for stream_id, schema in raw_schemas.items():
-        # TODO: populate any metadata and stream's key properties here..
-        stream_metadata = []
-        key_properties = []
+        # all streams get replicated by default
+        stream_metadata = {"inclusion": "automatic",
+                           "table-key-properties": ["id"],
+                           "replication_method": "FULL_TABLE"}
+        key_properties = ["airtable_id"]
         streams.append(
             CatalogEntry(
                 tap_stream_id=stream_id,
@@ -49,7 +124,8 @@ def discover():
                 replication_method=None,
             )
         )
-    return Catalog(streams)
+    catalog = Catalog(streams)
+    return catalog  # Catalog(streams)
 
 
 def sync(config, state, catalog):
@@ -68,7 +144,8 @@ def sync(config, state, catalog):
         )
 
         # TODO: delete and replace this inline function with your own data retrieval process:
-        tap_data = lambda: [{"id": x, "name": "row${x}"} for x in range(1000)]
+        def tap_data(): return [{"id": x, "name": "row${x}"}
+                                for x in range(1000)]
 
         max_bookmark = None
         for row in tap_data():
@@ -79,7 +156,8 @@ def sync(config, state, catalog):
             if bookmark_column:
                 if is_sorted:
                     # update bookmark to latest value
-                    singer.write_state({stream.tap_stream_id: row[bookmark_column]})
+                    singer.write_state(
+                        {stream.tap_stream_id: row[bookmark_column]})
                 else:
                     # if data unsorted, save max value until end of writes
                     max_bookmark = max(max_bookmark, row[bookmark_column])
@@ -92,17 +170,18 @@ def sync(config, state, catalog):
 def main():
     # Parse command line arguments
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+    config = args.config
 
     # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
-        catalog = discover()
+        catalog = discover(config)
         catalog.dump()
     # Otherwise run in sync mode
     else:
         if args.catalog:
             catalog = args.catalog
         else:
-            catalog = discover()
+            catalog = discover(config)
         sync(args.config, args.state, catalog)
 
 
